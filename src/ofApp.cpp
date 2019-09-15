aea750ae1eafaff8f82128399589b24d748ac779
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-15 18:50:48 -0400

    zip memory test works

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -15,7 +15,30 @@ void ofApp::update() {
     
     ofPixels pixels;
     fbo.readToPixels(pixels);
-    ofSaveImage(pixels, buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_LOW);
+    
+    int w = pixels.getWidth();
+    int h = pixels.getHeight();
+    
+    unsigned char pixelsRaw[pixels.size()];
+    std::copy(pixels.begin(), pixels.end(), pixelsRaw);
+
+    int n = sizeof(pixelsRaw) / sizeof(pixelsRaw[0]);
+
+    vector<unsigned char> pixelsRawVec(pixelsRaw, pixelsRaw+n);
+    cout << "orig size: " << sizeof(pixelsRaw) << endl;
+
+    vector<unsigned char> zippedFile = ofxZip::compress(pixelsRawVec);
+    cout << "compressed size: " << zippedFile.size() << endl;
+
+    vector<unsigned char> unZippedFile = ofxZip::uncompress(zippedFile);
+    cout << "restored size: " << unZippedFile.size() << endl;
+
+    unsigned char newPixelsRaw[unZippedFile.size()];
+    std::copy(unZippedFile.begin(), unZippedFile.end(), newPixelsRaw);
+    ofImage img;
+    img.allocate(w, h, OF_IMAGE_COLOR);
+    img.setFromPixels(newPixelsRaw, w, h, OF_IMAGE_COLOR);
+    ofSaveImage(img, buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_LOW);
     origImg.load(buffer);
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxZip.h"
 
 class ofApp : public ofBaseApp{
 
